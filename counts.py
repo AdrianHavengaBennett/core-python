97467fee5699ad16a2ea5296d1fb79679d4ae6a6
Adrian Havenga-Bennett <adrian.havenga@yahoo.com>
2019-10-05 13:37:29 +0000

    test

--- counts.py
+++ counts.py
@@ -1,9 +1,5 @@
 def count_upper_case(message):
-    count = 0
-    for c in message:
-        if c.isupper():
-            count += 1
-    return count
+    return sum([1 for c in message if c.isupper()])
 
 assert count_upper_case("") == 0, "Empty string"
 assert count_upper_case("A") == 1, "One upper case"
